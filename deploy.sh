c7b94dc21f1188b1d5656103339e0859debad6b4
Or Arbel <orarbel@gmail.com>
2019-07-26 18:48:56 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:reststate/reststate-vuex-docs.git master
+git push -f git@github.com:pickets/reststate-vuex-docs.git master
 
 cd -
